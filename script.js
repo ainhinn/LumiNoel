84de13051ae9397c6f063a587cbc5e32a3be643a
ainhinn <92919237+ainhinn@users.noreply.github.com>
2024-03-02 20:19:43 +0700

    script.js

--- script.js
+++ script.js
@@ -12,10 +12,9 @@ function jump(){
 var checkDead = setInterval(function() {
     var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft<200 && blockLeft>-20 && characterTop >= 50){
+    if(blockLeft<200 && blockLeft>-30 && characterTop >= 50){
         block.style.animation = "none";
-        alert("Noelle đã chạm được vào Lumine rồi nè <3");
-        // character.src ='LumineS.jpg';
+        alert("Sau " +Math.floor(counter/100) +" lần thì Noelle đã chạm được vào Lumine rồi nè <3");
         counter=0;
         block.style.animation = "block 1s infinite linear";
     }else{
